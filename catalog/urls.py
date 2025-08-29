a6d62da16875d5929b51ec540579dcfc79f3e20a
GeraMaldonado <gmaldonadofelix@gmail.com>
2025-08-29 11:21:03 -0600

    add: ruta para actualizar peliculas

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("", views.MovieList.as_view(), name="movie_list"),
     path("movies/<int:pk>/", views.MovieDetail.as_view(), name="movie_detail"),
     path("movies/create/", views.MovieCreate.as_view(), name="movie_create"),
+    path("movies/<int:pk>/edit/", views.MovieUpdate.as_view(), name="movie_update"),
+
 ]
 
